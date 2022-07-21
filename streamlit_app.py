7bd628768be7b5dfc338994e74794ded1c649745
Christopher Scott <109757350+cgscott84@users.noreply.github.com>
2022-07-21 14:01:12 -0400

    Update streamlit_app.py
    Fixed list again

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.subtitle ('Breakfast Menu')
-streamlit.list ('Omega 3 & Blueberry Oatmeal' 'Kale, Spinach & Rocket Smoothie'Up 'Hard-Boiled Free-Range Egg')
+streamlit.list ('Omega 3 & Blueberry Oatmeal'| 'Kale, Spinach & Rocket Smoothie' |'Hard-Boiled Free-Range Egg')
